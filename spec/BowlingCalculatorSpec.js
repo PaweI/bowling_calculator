b324ada92580c916501135b08b6123a4f0d68580
Pavel Redics <pavel.redics@gmail.com>
2014-11-28 20:23:19 +0000

    some tests

--- spec/BowlingCalculatorSpec.js
+++ spec/BowlingCalculatorSpec.js
@@ -4,29 +4,19 @@ describe("BowlingCalculator", function() {
     describe("one frame", function(){
       it("if player knocks less the 10 pins", function() {
         var bowling = new BowlingCalculator
-        expect(bowling.knocks(2,3)).toBe(5)
+        expect(bowling.shots(2,2,3)).toBe(5)
       });
 
       it("if player knocks 10 pins in first roll", function() {
         var bowling = new BowlingCalculator
-        expect(bowling.knocks(10)).toBe(10)
+        expect(bowling.shots(2,10)).toBe(10)
       });
 
-      // it("if player knocks 10 pins in first frame in roll one, and than 2 in each roll in second frame", function() {
-      //   var bowling = new BowlingCalculator
-      //   bowling.knocks(10)
-      //   bowling.knocks(2,2)
-      //   expect(bowling.shots(1)).toBe(14)
-      // });
-
-    });
-
-    describe("shows results", function() {
-      it("of shots being done", function() {
+      it("and then knocks 5, 4 and in roll1 and roll2 of the next frame", function() {
         var bowling = new BowlingCalculator
-        bowling.knocks(10)
-        // bowling.knocks(2,2)
-        expect(bowling.frames(1)).toBe("10")
+        bowling.shots(2,10)
+        bowling.shots(3,5,4)
+        expect(frames[2]["result"]).toBe(19)
       });
     });
 
--- src/BowlingCalculator.js
+++ src/BowlingCalculator.js
@@ -2,28 +2,38 @@ function BowlingCalculator() {
 
 };
 
-BowlingCalculator.prototype.knocks = function(shot1, shot2) {
-  roll1 = typeof shot1 !== 'undefined' ? shot1 : 0;
-  roll2 = typeof shot2 !== 'undefined' ? shot2 : 0;
-  return roll1 + roll2;
-};
+// BowlingCalculator.prototype.knocks = function(shot1, shot2) {
+//   roll1 = typeof shot1 !== 'undefined' ? shot1 : 0;
+//   roll2 = typeof shot2 !== 'undefined' ? shot2 : 0;
+//   return roll1 + roll2;
+// };
 
 
 var frames = {
-              1 : {roll1 : null, roll2 : null},
-              2 : {roll1 : null, roll2 : null},
-              3 : {roll1 : null, roll2 : null}
-              4 : {roll1 : null, roll2 : null}
-              5 : {roll1 : null, roll2 : null}
-              6 : {roll1 : null, roll2 : null}
-              7 : {roll1 : null, roll2 : null}
-              8 : {roll1 : null, roll2 : null}
-              9 : {roll1 : null, roll2 : null}
-              10 : {roll1 : null, roll2 : null}
+              1: {roll1: null, roll2: null, result: null},
+              2: {roll1: null, roll2: null, result: null},
+              3: {roll1: null, roll2: null, result: null},
+              4: {roll1: null, roll2: null, result: null},
+              5: {roll1: null, roll2: null, result: null},
+              6: {roll1: null, roll2: null, result: null},
+              7: {roll1: null, roll2: null, result: null},
+              8: {roll1: null, roll2: null, result: null},
+              9: {roll1: null, roll2: null, result: null},
+             10: {roll1: null, roll2: null, result: null}
             };
 
-BowlingCalculator.prototype.shots = function(frame) {
-  frames[shot][roll1]
+BowlingCalculator.prototype.shots = function(frame, shot1, shot2) {
+  shot1 = typeof shot1 !== 'undefined' ? shot1 : 0;
+  shot2 = typeof shot2 !== 'undefined' ? shot2 : 0;
+  frames[frame]["roll1"] = shot1
+  frames[frame]["roll2"] = shot2
+  frames[frame]["result"] = shot1+shot2
+  if(frames[frame-1]["roll1"] === 10 ) {
+    frames[frame-1]["result"] += shot1+shot2
+  };
 
-
+  console.log(frames)
+  return shot1 + shot2
 };
+
+
