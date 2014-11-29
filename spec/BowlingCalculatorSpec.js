0cb7cf432d00ac645eccc8dd84b9c32607f0873d
Pavel Redics <pavel.redics@gmail.com>
2014-11-29 16:09:48 +0000

    added more tests

--- spec/BowlingCalculatorSpec.js
+++ spec/BowlingCalculatorSpec.js
@@ -1,26 +1,51 @@
 describe("BowlingCalculator", function() {
 
+  var bowling
+
+  beforeEach(function() {
+    bowling = new BowlingCalculator
+  });
+
   describe("calculates", function() {
     describe("one frame", function(){
       it("if player knocks less the 10 pins", function() {
-        var bowling = new BowlingCalculator
         expect(bowling.shots(1,2,3)).toBe(5)
       });
 
       it("if player knocks 10 pins in first roll", function() {
-        var bowling = new BowlingCalculator
         expect(bowling.shots(1,10)).toBe(10)
       });
 
       it("and then knocks 5, 4 and in roll1 and roll2 of the next frame", function() {
-        var bowling = new BowlingCalculator
         bowling.shots(1,10)
-        bowling.shots(2,5,4)
-        expect(frames[1]["result"]).toBe(19)
+        expect(bowling.shots(2,5,4)).toBe(28)
+      });
+
+      it("if player knocks 10 pins in first frame in two rolls and than 5 in next roll", function() {
+        bowling.shots(1,3,7)
+        expect(bowling.shots(2,5,2)).toBe(22)
+      });
+
+      it("if player knocks 10 pins in first and second frames in roll one and than 5 in roll one of frame 3", function() {
+        bowling.shots(1,10)
+        bowling.shots(2,10)
+        expect(bowling.shots(3,5,4)).toBe(53)
       });
     });
 
+    describe("second frame", function() {
+      it("when player knocks 7 pins in first frame and 5 in second", function() {
+        bowling.shots(1,5,2)
+        expect(bowling.shots(2,3,2)).toBe(12)
+      });
+    });
+
+    describe("frames from 1 to 10", function() {
+      it("when player knocks 7 in first frame", function() {
+        bowling.shots(1,5,2)
+        expect();
+      });
+
+    });
   });
-
-
 });
--- src/BowlingCalculator.js
+++ src/BowlingCalculator.js
@@ -2,13 +2,6 @@ function BowlingCalculator() {
 
 };
 
-// BowlingCalculator.prototype.knocks = function(shot1, shot2) {
-//   roll1 = typeof shot1 !== 'undefined' ? shot1 : 0;
-//   roll2 = typeof shot2 !== 'undefined' ? shot2 : 0;
-//   return roll1 + roll2;
-// };
-
-
 var frames = {
               1: {roll1: null, roll2: null, result: null},
               2: {roll1: null, roll2: null, result: null},
@@ -29,11 +22,28 @@ BowlingCalculator.prototype.shots = function(frame, shot1, shot2) {
   frames[frame]["roll2"] = shot2
   frames[frame]["result"] = shot1+shot2
   if(frame-1>0) {
+
+    if(frame-2>0) {
+      if(frames[frame-1]["roll1"] === 10 && frames[frame-2]["roll1"] === 10) {
+        frames[frame-2]["result"] += shot1
+        // frames[frame-1]["result"] += shot1+shot2
+      };
+    };
+
     if(frames[frame-1]["roll1"] === 10 ) {
       frames[frame-1]["result"] += shot1+shot2
     };
+
+    if((frames[frame-1]["roll1"] + frames[frame-1]["roll2"] === 10) && frames[frame-1]["roll2"] !== 0) {
+      frames[frame-1]["result"] += shot1
+    };
   };
-  return shot1 + shot2
+
+  if(frame-1>0) {
+    frames[frame]["result"] += frames[frame-1]["result"]
+  };
+
+  return frames[frame]["result"]
 };
 
 
