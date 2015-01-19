23ae32ba68e6f9fb16dad6488a95632d5dbd1a4b
Pavel Redics <pavel.redics@gmail.com>
2015-01-19 19:46:53 +0000

    designing gutter game

--- spec/BowlingCalculatorSpec.js
+++ spec/BowlingCalculatorSpec.js
@@ -3,100 +3,15 @@ describe("BowlingCalculator", function() {
   var bowling
 
   beforeEach(function() {
-    bowling = new BowlingCalculator
+    bowling = new BowlingCalculator;
   });
 
   describe("calculates", function() {
-    describe("one frame", function(){
-      it("if player knocks less the 10 pins", function() {
-        expect(bowling.shots(1,2,3)).toBe(5)
-      });
-
-      it("if player knocks 5 pins in first frame and 5 in second", function() {
-        bowling.shots(1,3,2)
-        expect(bowling.shots(2,2,3)).toBe(10)
-      });
-
-      it("if player knocks 10 pins in first roll", function() {
-        expect(bowling.shots(1,10)).toBe(10)
-      });
-
-      it("and then knocks 5, 4 and in roll1 and roll2 of the next frame", function() {
-        bowling.shots(1,10)
-        expect(bowling.shots(2,5,4)).toBe(28)
-      });
-
-      it("if player knocks 10 pins in first frame in two rolls and than 5 in next roll", function() {
-        bowling.shots(1,3,7)
-        expect(bowling.shots(2,5,2)).toBe(22)
-      });
-
-      it("if player knocks 10 pins in first and second frames in roll one and than 5 in roll one of frame 3", function() {
-        bowling.shots(1,10)
-        bowling.shots(2,10)
-        expect(bowling.shots(3,5,4)).toBe(53)
-      });
-    });
-
-    describe("second frame", function() {
-      it("when player knocks 7 pins in first frame and 5 in second", function() {
-        bowling.shots(1,5,2)
-        expect(bowling.shots(2,3,2)).toBe(12)
-      });
-    });
-
-    describe("all ten frames", function() {
-      it("when player knocks 7 in all frames first frame", function() {
-        bowling.shots(1,5,2)
-        bowling.shots(2,5,2)
-        bowling.shots(3,5,2)
-        bowling.shots(4,5,2)
-        bowling.shots(5,5,2)
-        bowling.shots(6,5,2)
-        bowling.shots(7,5,2)
-        bowling.shots(8,5,2)
-        bowling.shots(9,5,2)
-        expect(bowling.shots(10,5,2)).toBe(70)
-      });
-
-      it("when player does couple of strikes in a row", function() {
-        bowling.shots(1,5,2)
-        bowling.shots(2,5,2)
-        bowling.shots(3,5,2)
-        bowling.shots(4,5,2)
-        bowling.shots(5,5,2)
-        bowling.shots(6,10)
-        bowling.shots(7,10)
-        bowling.shots(8,5,2)
-        bowling.shots(9,5,2)
-        expect(bowling.shots(10,5,2)).toBe(98)
-      });
-
-      it("when player does some spares and strike", function() {
-        bowling.shots(1,5,2)
-        bowling.shots(2,5,5)
-        bowling.shots(3,2,2)
-        bowling.shots(4,3,2)
-        bowling.shots(5,3,2)
-        bowling.shots(6,10)
-        bowling.shots(7,10)
-        bowling.shots(8,5,2)
-        bowling.shots(9,5,2)
-        expect(bowling.shots(10,5,2)).toBe(96)
-      });
-
-      it("when player does all the strikes", function () {
-        bowling.shots(1,10)
-        bowling.shots(2,10)
-        bowling.shots(3,10)
-        bowling.shots(4,10)
-        bowling.shots(5,10)
-        bowling.shots(6,10)
-        bowling.shots(7,10)
-        bowling.shots(8,10)
-        bowling.shots(9,10)
-        expect(bowling.shots(10,10,10,10)).toBe(300)
-      });
+    it("Gutter game, when player didn't hit any pin at any frame", function() {
+      bowling.gutterGame()
+      expect(bowling.frame1.roll1).toBe(0)  
+      expect(bowling.frame1.roll2).toBe(0)  
+      expect(bowling.frame1.total).toBe(0)  
     });
   });
 });
--- src/BowlingCalculator.js
+++ src/BowlingCalculator.js
@@ -1,51 +1,13 @@
 function BowlingCalculator() {
+  this.frame1 = {roll1: null, roll2: null, total: null};
 
 };
 
-var frames = {
-              1: {roll1: null, roll2: null, result: null},
-              2: {roll1: null, roll2: null, result: null},
-              3: {roll1: null, roll2: null, result: null},
-              4: {roll1: null, roll2: null, result: null},
-              5: {roll1: null, roll2: null, result: null},
-              6: {roll1: null, roll2: null, result: null},
-              7: {roll1: null, roll2: null, result: null},
-              8: {roll1: null, roll2: null, result: null},
-              9: {roll1: null, roll2: null, result: null},
-             10: {roll1: null, roll2: null, roll3: null, result: null}
-            };
-
-
-BowlingCalculator.prototype.shots = function(frame, shot1, shot2, shot3) {
-  var shot1 = typeof shot1 !== 'undefined' ? shot1 : 0;
-  var shot2 = typeof shot2 !== 'undefined' ? shot2 : 0;
-  var shot3 = typeof shot3 !== 'undefined' ? shot3 : 0;
-  frames[frame]["roll1"] = shot1;
-  frames[frame]["roll2"] = shot2;
-  frames[frame]["result"] = shot1+shot2+shot3;
-
-  if(frame>=3) {
-    if(frames[frame-2]["roll1"] === 10 && frames[frame-1]["roll1"] === 10) {
-      frames[frame-2]["result"] += shot1;
-      frames[frame-1]["result"] += shot1;
-    };
-  };
-
-  if(frame-1>0) {
-
-    if(frames[frame-1]["roll1"] === 10) {
-      frames[frame-1]["result"] += shot1+shot2;
-    };
-
-    if(frames[frame-1]["roll1"] + frames[frame-1]["roll2"] === 10 && frames[frame-1]["roll2"] !== 0) {
-      frames[frame-1]["result"] += shot1;
-    };
-
-    frames[frame]["result"] += frames[frame-1]["result"];
-
-  };
-
-  return frames[frame]["result"];
+BowlingCalculator.prototype.gutterGame = function() {
+  this.frame1.roll1 = 0;
+  this.frame1.roll2 = 0;
+  this.frame1.total = 0;
 };
+
 
 
